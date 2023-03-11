77ebf0735a61d9da99af48a38157a63993d7eca7
Stephen Henley <2618252h@student.gla.ac.uk>
2023-03-11 14:15:59 +0000

    Game and category models

--- GameRate/gamerateapp/models.py
+++ GameRate/gamerateapp/models.py
@@ -1,3 +1,36 @@
 from django.db import models
+from django.template.defaultfilters import slugify
 
 # Create your models here.
+class Game(models.Model):
+    name = models.CharField(max_length=128, unique=True)
+    publisher = models.CharField(max_length=128)
+    game_Description = model.CherField(max_length=128)
+    category = models.ForeignKey(Category, on_delete=models.CASCADE)
+    story_rating = models.IntegerField(default = 0)
+    gameplay_rating = models.IntegerField(default = 0)
+    graphics_rating = models.IntegerField(default = 0)
+    difficulty_rating = models.IntegerField(default = 0)
+    picture = models.ImageField(upload_to='game_images', blank=True)
+    slug = models.SlugField(unique=True)
+    
+    def save(self, *args, **kwargs):
+        self.slug = slugify(self.name)
+        super(Game, self).save(*args, **kwargs)
+        
+    class Meta:
+        verbose_name_plural = 'Categories'
+    
+    def __str__(self):
+        return self.name
+        
+class Category(models.Model):
+    name = models.CharField(max_lenght=128, unique=True)
+    slug = models.SlugField(unique=True)
+    
+    def save(self, *args, **kwargs):
+        self.slug = slugify(self.name)
+        super(Game, self).save(*args, **kwargs)
+    
+    def __str__(self):
+        return self.name
--- GameRate/gamerateapp/views.py
+++ GameRate/gamerateapp/views.py
@@ -1,19 +1,15 @@
 from django.shortcuts import render
 from django.http import HttpResponse
-
+from gamerateapp.models import Game
+from gamerateapp.models import Category
 # Create your views here.
 
 def index(request):
 
-    top_gameplay = Game.objects.order_by('-gameplayrating')[:1]
-    top_graphics = Game.objects.order_by('-graphicsrating')[:1]
-    top_story = Game.objects.order_by('-storyrating')[:1]
-    top_difficulty = Game.objects.order_by('-difficultyrating')[:1]
-    
-    gameplay_publisher = top_gameplay.publisher
-    graphics_publisher = top_graphics.publisher
-    story_publisher = top_story.publisher
-    difficulty_publisher = top_difficulty.publisher
+    top_gameplay = Game.objects.order_by('-gameplay_rating')[:1]
+    top_graphics = Game.objects.order_by('-graphics_rating')[:1]
+    top_story = Game.objects.order_by('-story_rating')[:1]
+    top_difficulty = Game.objects.order_by('-difficulty_rating')[:1]
     
     context_dict = {}
     context_dict['top_gameplay'] = top_gameplay
@@ -21,21 +17,16 @@ def index(request):
     context_dict['top_story'] = top_story
     context_dict['top_difficulty'] = top_difficulty
     
-    context_dict['gameplay_publisher'] = gameplay_publisher
-    context_dict['graphics_publisher'] = graphics_publisher
-    context_dict['story_publisher'] = story_publisher
-    context_dict['difficulty_publisher'] = difficulty_publisher
-    
     response = render(request, 'gamerateapp/index.html', context=context_dict)
     
     return response
     
 def categories(request):
+
+    context_dict = {}
     
-    
-
-
-
+    for categeroy in Category.objects.all():
+        context_dict[category.str()] = Game.objects.filter(category = category)[:1].picture 
     
 
     response = render(request, 'gamerateapp/categories.html', context=context_dict)
@@ -44,13 +35,36 @@ def categories(request):
     
 def category(request, category_name_slug):
 
+    context_dict = {}
     
-
+    try:
+        category = Category.objects.get(slug=category_name_slug)
+        games = Game.objects.filter(category = category)
+        
+        context_dict['games'] = games
+        context_dict['category'] = category
+        
+    except Category.DoesNotExist:
+        context_dict['category'] = None
+        context_dict['games'] = None
+    
+    
     response = render(request, 'gamerateapp/category.html', context=context_dict)
     
     return response
     
 def game(request, game_name_slug):
+
+    context_dict = {}
+
+    try:
+        game = Game.objects.get(slug=game_name_slug)
+        
+        context_dict['game'] = game
+        
+    except Game.DoesNotExist:
+        context_dict['game'] = None
+        
     
     
     response = render(request, 'gamerateapp/game.html', context=context_dict)
